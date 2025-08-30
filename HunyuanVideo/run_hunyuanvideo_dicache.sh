a3d6a272b40b7553ba00207868dc5d8d815e7960
Azusa <3351506128@qq.com>
2025-08-30 14:59:08 +0800

    Add files via upload

---
+++ HunyuanVideo/run_hunyuanvideo_dicache.sh
@@ -0,0 +1,8 @@
+python3 sample_dicache.py \
+--video-size 544 960 \
+--video-length 129 \
+--infer-steps 50 \
+--prompt "a  realistic young expert man speaking with enthusiasm front of camera with vintage toy background." \
+--flow-reverse \
+--save-path ./dicache_results_long \
+--seed 0
